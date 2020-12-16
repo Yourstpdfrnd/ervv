d8368ce9e86f70fe350348d42588b239f049ecb1
Yourstpdfrnd <65391216+Yourstpdfrnd@users.noreply.github.com>
2020-12-16 22:50:50 +0300

    added header

---
+++ src/js/_vars.js
@@ -0,0 +1,8 @@
+export default {
+  $window: window,
+  $document: document,
+  $html: document.documentElement,
+  $body: document.body,
+  $burger: document.querySelector('.burger'),
+  $navMenu: document.querySelector('.header__menu'),
+}
---
+++ src/js/components/burger.js
@@ -0,0 +1,5 @@
+import vars from '../_vars';
+
+vars.$burger.addEventListener('click', () => {
+   vars.$navMenu.classList.toggle('active')
+});
